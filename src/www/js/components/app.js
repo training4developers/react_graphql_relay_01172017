3426dbc7abbeb99e4ca3303cccab59d38d90e7eb
Eric W. Greene <eric@ericwgreene.com>
2017-01-18 09:47:04 -0800

    added code demos

---
+++ src/www/js/components/app.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export class App extends React.Component {
+
+    static propTypes = {
+        message: React.PropTypes.string,
+        books: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                id: null, title: null, category: null, price: null 
+            })
+        )
+    };
+
+    render() {
+        return <div>
+            <h1>{this.props.message}</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.books.map(book => <tr key={book.id}>
+                        <td>{book.title}</td>
+                        <td>{book.category}</td>
+                        <td>{book.price}</td>
+                    </tr>)}
+                </tbody>
+            </table>
+        </div>;
+    }
+}
