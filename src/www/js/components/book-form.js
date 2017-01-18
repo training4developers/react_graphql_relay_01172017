e8ca6fcdee50203ea6f160d86bb01309df9d5caf
Eric W. Greene <eric@ericwgreene.com>
2017-01-18 12:03:30 -0800

    added code demos

--- src/www/js/components/book-form.js
+++ src/www/js/components/book-form.js
@@ -11,8 +11,6 @@ export class BookForm extends React.Component {
             price: 0,
             authorId: 0
         };
-
-        //this.onChange = this.onChange.bind(this);
     }
 
     onChange = (e) => {
@@ -21,20 +19,37 @@ export class BookForm extends React.Component {
         });
     };
 
-    // onChange(e) {
-    //     this.setState({
-    //         [e.target.name]: e.target.value
-    //     });        
-    // }
+    onClick = () => {
+        console.log(this.state);
+    };
 
     render() {
-        return <form>
-            <div>
-                <label htmlFor="book-title">Title</label>
-                <input type="text" id="book-title" name="title"
-                    value={this.state.title} onChange={this.onChange} />
-            </div>
-        </form>;
+        return <div>
+            <h2>Create a Book</h2>
+            <form>
+                <div>
+                    <label htmlFor="book-title">Title</label>
+                    <input type="text" id="book-title" name="title"
+                        value={this.state.title} onChange={this.onChange} />
+                </div>
+                <div>
+                    <label htmlFor="book-category">Category</label>
+                    <input type="text" id="book-category" name="category"
+                        value={this.state.category} onChange={this.onChange} />
+                </div>
+                <div>
+                    <label htmlFor="book-price">Price</label>
+                    <input type="text" id="book-price" name="price"
+                        value={this.state.price} onChange={this.onChange} />
+                </div>
+                <div>
+                    <label htmlFor="book-author-id">Author ID</label>
+                    <input type="text" id="book-author-id" name="authorId"
+                        value={this.state.authorId} onChange={this.onChange} />
+                </div>
+                <button type="button" onClick={this.onClick}>Add Book</button>
+            </form>
+        </div>;
     }
 
 }
